ef14451735bd35b09d9365c2b61c1f9020472d07
Felipe Peña <felipe@penya.cl>
2020-04-02 09:46:56 -0700

    Finish part 1

---
+++ database/migrations/2020_04_02_062648_create_mood_updates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoodUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mood_updates', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('mood');
+            $table->text('journal')->nullable();
+            $table->text('tags')->nullable();
+
+            $table->unsignedInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mood_updates');
+    }
+}
